b1925e58ac631d877e3de4b0832d9fdb4532e56e
Zuldajri <w.matyas15@hotmail.fr>
2022-01-03 19:15:43 +0100

    Update initialize-cloudera-server.sh

--- scripts/initialize-cloudera-server.sh
+++ scripts/initialize-cloudera-server.sh
@@ -59,12 +59,42 @@ sed -i 's^SELINUX=enforcing^SELINUX=disabled^g' /etc/selinux/config || true
 cat /etc/selinux/config > /tmp/afterSeLinux.out
 log "Done disabling selinux"
 
+
+# Set up python
+yum install -y epel-release >> "${LOG_FILE}" 2>&1
+yum -y install python-pip >> "${LOG_FILE}" 2>&1
+pip install --upgrade pip
+pip install cm_client >> "${LOG_FILE}" 2>&1
+
+cp /usr/lib/systemd/system/rngd.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl start rngd
+systemctl enable rngd
+
+echo "-- Installing requirements for Stream Messaging Manager"
+yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash - 
+yum install nodejs -y
+npm install forever -g 
+
+echo "-- Configure networking"
+PUBLIC_IP=`curl https://api.ipify.org/`
+hostnamectl set-hostname `hostname -f`
+echo "`hostname -I` `hostname`" >> /etc/hosts
+sed -i "s/HOSTNAME=.*/HOSTNAME=`hostname`/" /etc/sysconfig/network
+systemctl disable firewalld
+systemctl stop firewalld
+setenforce 0
+sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+
+
+echo "-- Install CM and MariaDB"
+
 set +e
 
-log "Set cloudera-manager.repo to CM v6"
-yum clean all >> "${LOG_FILE}" 2>&1
-rpm --import https://archive.cloudera.com/cm6/6.3.1/redhat7/yum/RPM-GPG-KEY-cloudera >> "${LOG_FILE}" 2>&1
-wget https://archive.cloudera.com/cm6/6.3.1/redhat7/yum/cloudera-manager.repo -O /etc/yum.repos.d/cloudera-manager.repo >> "${LOG_FILE}" 2>&1
+log "Set cloudera-manager.repo to CM v7"
+
+wget https://archive.cloudera.com/cm7/7.4.4/redhat7/yum/cloudera-manager-trial.repo -P /etc/yum.repos.d/ >> "${LOG_FILE}" 2>&1
 
 ## MariaDB 10.1
 cat - >/etc/yum.repos.d/MariaDB.repo <<EOF
@@ -122,46 +152,51 @@ log "-- Prepare CM database 'scm'"
 log "finished installing external DB"
 #######################################################################################################################
 
+## PostgreSQL see: https://www.postgresql.org/download/linux/redhat/
+yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+yum install -y postgresql96
+yum install -y postgresql96-server
+pip install psycopg2==2.7.5 --ignore-installed
+
+echo 'LC_ALL="en_US.UTF-8"' >> /etc/locale.conf
+/usr/pgsql-9.6/bin/postgresql96-setup initdb
+
+cat pg_hba.conf > /var/lib/pgsql/9.6/data/pg_hba.conf
+cat postgresql.conf > /var/lib/pgsql/9.6/data/postgresql.conf
+
+
+echo "--Enable and start pgsql"
+systemctl enable postgresql-9.6
+systemctl start postgresql-9.6
+
+echo "-- Create DBs required by CM"
+sudo -u postgres psql <<EOF 
+CREATE DATABASE ranger;
+CREATE USER ranger WITH PASSWORD 'cloudera';
+GRANT ALL PRIVILEGES ON DATABASE ranger TO ranger;
+CREATE DATABASE das;
+CREATE USER das WITH PASSWORD 'cloudera';
+GRANT ALL PRIVILEGES ON DATABASE das TO das;
+EOF
+
+
+
+
+
 log "-- Install CSDs"
 # install local CSDs
 mv ~/*.jar /opt/cloudera/csd/
-
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFI-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFICA-1.9.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/CFM/csd/1.0.0.0/NIFIREGISTRY-0.3.0.1.0.0.0-90.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/cdsw1/1.6.0/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.0.jar -P /opt/cloudera/csd/
-# CSD for C5
-wget https://archive.cloudera.com/cdsw1/1.6.0/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH5-1.6.0.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/spark2/csd/SPARK2_ON_YARN-2.4.0.cloudera1.jar -P /opt/cloudera/csd/
+mv /home/centos/*.jar /opt/cloudera/csd/
 
 chown cloudera-scm:cloudera-scm /opt/cloudera/csd/*
 chmod 644 /opt/cloudera/csd/*
 
 log "-- Install local parcels"
 mv ~/*.parcel ~/*.parcel.sha /opt/cloudera/parcel-repo/
+mv /home/centos/*.parcel /home/centos/*.parcel.sha /opt/cloudera/parcel-repo/
 chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo/*
 
-log "-- Install CEM Tarballs"
-mkdir -p /opt/cloudera/cem
-wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
-ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
-ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
-ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
-chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
-chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
-chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
-rm -f /opt/cloudera/cem/efm/conf/efm.properties
-cp efm.properties /opt/cloudera/cem/efm/conf
-rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
-cp bootstrap.conf /opt/cloudera/cem/minifi/conf
-sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/efm/conf/efm.properties
-sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/minifi/conf/bootstrap.conf
-/opt/cloudera/cem/minifi/bin/minifi.sh install
+
 
 log "-- Enable passwordless root login via rsa key"
 ssh-keygen -f ~/myRSAkey -t rsa -N ""
@@ -185,11 +220,9 @@ log "-- Now CM is started and the next step is to automate using the CM API"
 log "END: master node deployments"
 
 
-# Set up python
-yum install -y epel-release >> "${LOG_FILE}" 2>&1
-yum -y install python-pip >> "${LOG_FILE}" 2>&1
-pip install --upgrade pip
-pip install cm_client >> "${LOG_FILE}" 2>&1
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+python ./get-pip.py
+pip install --upgrade cm_client
 
 
 
@@ -202,41 +235,7 @@ sed -i "s/passe/$Password/g" /var/lib/waagent/custom-script/download/1/${TSHIRTS
 
 python /var/lib/waagent/custom-script/download/1/${TSHIRTSIZE}create_cluster.py /var/lib/waagent/custom-script/download/1/${TSHIRTSIZE}default_template.json
 
-# configure and start EFM and Minifi
-service efm start
-#service minifi start
 
 
 
-#original
 
-## trap file to indicate done
-
-#log "creating file to indicate finished"
-#touch /tmp/readyFile
-
-## Execute script to deploy Cloudera cluster
-#log "BEGIN: CM deployment - starting"
-#log "Parameters: $ClusterName $mip $worker_ip $VMSIZE"
-#status=0
-#if $HA; then
-#    python cmxDeployOnIbiza.py -n "$ClusterName" -u "$User" -p "$Password" -m "$mip" -w "$worker_ip" -a -c "$cmUser" -s "$cmPassword" -v "$VMSIZE" >> "${LOG_FILE}" 2>&1
-#    status=$?
-#else
-#    python cmxDeployOnIbiza.py -n "$ClusterName" -u "$User" -p "$Password" -m "$mip" -w "$worker_ip" -c "$cmUser" -s "$cmPassword" -v "$VMSIZE" >> "${LOG_FILE}" 2>&1
-#    status=$?
-#fi
-
-#log "END: CM deployment ended with status '$status'"
-#log "-- At this point you can login into Cloudera Manager host on port 7180 and follow the deployment of the cluster"
-
-#if [ $status -eq 0 ]
-#then
-#    log "------- initialize-cloudera-server.sh succeeded -------" 
-#    # always `exit 0` on success
-#    exit 0
-
-#else
-#    log "------- initialize-cloudera-server.sh failed -------" 
-#    exit 1
-#fi
